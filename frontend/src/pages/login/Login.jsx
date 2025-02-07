a48c31af5d00f8c52e2407e3ed27714e17bdcb56
creait02 <j.montilla@corpocrea.com>
2025-02-07 16:34:36 -0400

    💄 View of the login finish

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Input, FormControl, FormLabel, Box, VStack } from '@chakra-ui/react';
+import { Image, Stack } from '@chakra-ui/react'
 // import { useHistory } from 'react-router';
 
 const LoginForm = () => {
@@ -10,7 +11,8 @@ const LoginForm = () => {
 
   const handleLogin = () => {
     if (username === 'admin' && password === 'admin123') {
-      history.push('/dashboard');  // Redirige al dashboard
+      //history.push('/dashboard');  // Redirige al dashboard
+      console.log('Funciona');
     } else {
       setError('Credenciales incorrectas');
     }
@@ -18,8 +20,16 @@ const LoginForm = () => {
 
   return (
     <>
-      <h2 className='text-6xl_ font-bold underline'>Inicia Sesión</h2>
-      <Box maxW="sm" mx="auto" mt="10">
+    <div className='w-full h-full flex flex-col justify-center items-center mt-10'>
+      <Stack className='w-full flex justify-center items-center py-8'>
+        <Image
+          objectFit="cover"
+          src="https://seeklogo.com/images/V/VENEZOLANA_DE_TELEVISION-logo-02E35AB2EA-seeklogo.com.png"
+        />
+      </Stack>
+      <h2 className='text-4xl! font-bold'>Inicia Sesión</h2>
+
+      <Box maxW="sm" mx="auto" mt="10" display="flex" justifyContent="center" alignItems="center">
         <VStack spacing={4} align="flex-start">
           <FormControl>
             <FormLabel>Usuario</FormLabel>
@@ -43,11 +53,13 @@ const LoginForm = () => {
 
           {error && <Box color="red.500">{error}</Box>}
 
-          <Button colorScheme="blue" onClick={handleLogin}>
+          <Button className='w-full' colorScheme="red" onClick={handleLogin}>
             Iniciar Sesión
           </Button>
+          <p className='pt-5'>Sistema Lunch 2025 - Venezolana de Televisión</p>
         </VStack>
       </Box>
+    </div>
     </>
   );
 };
