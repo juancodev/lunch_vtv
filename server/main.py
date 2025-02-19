9b1477c6e187fcce65fd4e18177fd9b825dcb4bc
creait02 <j.montilla@corpocrea.com>
2025-02-19 17:22:59 -0400

    🚧 Add database and new packages

--- server/main.py
+++ server/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-
-
-
+from config.database import engine, Base
+# from middleware.error_handler import ErrorHandler
+from router.auth import auth_router
 
 app = FastAPI()
 
 app.title = "Lunch VTV"
 app.version = "0.0.1"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# app.add_middleware(ErrorHandler)
+
+app.include_router(auth_router)
+
+Base.metadata.create_all(bind=engine)
--- server/router/auth.py
+++ server/router/auth.py
@@ -6,6 +6,9 @@ auth_router = APIRouter()
 
 @auth_router.post("/auth/login", tags=["auth"], response_model=AuthUser)
 def login(auth_user: AuthUser):
+
+     
+
     return {
         "email": auth_user.email,
         "password": auth_user.password,
