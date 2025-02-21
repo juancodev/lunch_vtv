17a8ecfc1c4b44cf105da1a752f00537285f0fde
creait02 <j.montilla@corpocrea.com>
2025-02-21 14:04:57 -0400

    🚧 Delete folder of python

--- server_test/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from router.auth import auth_router
-
-app = FastAPI()
-
-app.title = "Lunch VTV"
-app.version = "0.0.1"
-
-app.include_router(auth_router)
--- server_test/models/user_admin.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class UserAdmin(BaseModel):
-    id: str
-    email: str
-    password: str
-    is_active: bool
